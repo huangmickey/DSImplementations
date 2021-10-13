1ce2d358031684841a0cfc9c292f9015735ac29c
huangmickey <67403573+huangmickey@users.noreply.github.com>
2021-10-12 18:41:43 -0700

    Added new data structures to work on

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,11 +1,23 @@
 public class LinkedList {
 
     private class Node {
+        private int data;
+        private Node next;
 
-
+        public Node(int data) {
+            this.data = data;
+        }
 
     }
 
+    private Node head;
 
 
+    public void add() {
+
+    }
+
+    public void remove() {
+
+    }
 }
