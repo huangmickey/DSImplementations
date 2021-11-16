68fd245cd0f65f2382585e6a3e94a40372cde0e7
huangmickey <916m.huang@gmail.com>
2021-11-15 17:31:25 -0800

    Implemented singly linked list with two tails (head,tail)

--- src/LinkedList.java
+++ src/LinkedList.java
@@ -1,13 +1,30 @@
 import java.util.NoSuchElementException;
 
+/**
+ * Linked lists grow and shrink automatically. In a linked list, nodes are linked together. A linked list node contain two fields,
+ * value and a Node which it points to. Usually there is a head and a tail node which are the first node and last nodes of a linked list.
+ *
+ * Memory Address of each node can be all over the place, therefore we need each node to reference anotehr node.
+ * Time complexity:
+ * Lookup: By Value --> O(n) because the last value
+ *         By Index --> Not possible, have to traverse list until value is found O(n)
+ * Insert:
+ *         At the End       --> O(1)    :   Tail points to new node at end
+ *         At the Beginning --> O(1)    :   Head points to new node at beginning
+ *         In the middle    --> O(n)
+ * Delete:
+ *         From the Beginning   --> O(1)
+ *         From the End         --> O(n)    : Have to get previous node before the last in order to delete last node
+ *         From the Middle      --> O(n)
+ */
 public class LinkedList {
 
     private class Node {
-        private int data;
+        private int value;
         private Node next;
 
-        public Node(int data) {
-            this.data = data;
+        public Node(int value) {
+            this.value = value;
             this.next = null;
         }
 
@@ -16,8 +33,8 @@ public class LinkedList {
     private Node head;
 
     // O(N) time complexity --> Adding new node at end of linked list requires traversing N node elements until the end before adding new node
-    public void add(int data) {
-        Node nextNode = new Node(data);
+    public void add(int value) {
+        Node nextNode = new Node(value);
 
         if (isEmpty()) {
             head = nextNode;
@@ -38,7 +55,7 @@ public class LinkedList {
             prev = currNode;
             currNode = currNode.next;
         }
-        int removedData = currNode.data;
+        int removedData = currNode.value;
         prev.next = null;
         return removedData;
 
@@ -59,7 +76,7 @@ public class LinkedList {
             if (targetNode == null) throw new NoSuchElementException();
             i++;
         }
-        int removedData = targetNode.data;
+        int removedData = targetNode.value;
         prev.next = targetNode.next;
         return removedData;
     }
@@ -70,6 +87,5 @@ public class LinkedList {
         list.add(2);
         list.add(3);
         System.out.println(list.remove());
-        
     }
 }
---
+++ src/TwoTailLinkedList.java
@@ -0,0 +1,205 @@
+import java.util.Arrays;
+import java.util.NoSuchElementException;
+
+public class TwoTailLinkedList {
+
+    private class Node {
+        private int value;
+        private Node next;
+
+
+        public Node(int value) {
+            this.value = value;
+        }
+    }
+
+
+    private Node first;
+    private Node last;
+    private int size;
+    //  addFirst
+    //  addLast
+    //  deleteFirst
+    //  deleteLast
+    // contains
+    // indexOf
+    public void addFirst(int value) {
+        Node node = new Node(value);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            node.next = first;
+            first = node;
+        }
+        size++;
+    }
+    public void addLast(int value) {
+        Node node = new Node(value);
+
+        if (isEmpty())
+            first = last = node;
+        else {
+            last.next = node;
+            last = node;
+        }
+        size++;
+    }
+
+    public boolean contains(int item) {
+        return indexOf(item) != -1;
+    }
+
+
+    public void removeFirst() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        Node second = first.next;
+        first.next = null;
+        first = second;
+        size--;
+    }
+
+    public void removeLast() {
+        if (isEmpty()) throw new NoSuchElementException();
+
+        if (first == last) {
+            first = last = null;
+            return;
+        }
+
+        Node prev = getPrevious(last);
+        last = prev;
+        last = last.next;
+        size--;
+    }
+
+    private Node getPrevious(Node node) {
+        Node curr = first;
+        while (curr != null) {
+            if (curr.next == node) return curr;
+            curr = curr.next;
+        }
+        return null;
+    }
+
+    public int indexOf(int value) {
+        int index = 0;
+        Node curr = first;
+        while (curr != null) {
+            if (curr.value == value) return index;
+            curr = curr.next;
+            index++;
+        }
+        return -1;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public int[] toArray() {
+
+        int[] array = new int[size];
+        Node curr = first;
+        int index = 0;
+        while (curr != null) {
+            array[index++] = curr.value;
+            curr = curr.next;
+        }
+        return array;
+    }
+
+    public void reverse() {
+        if (isEmpty()) return;
+        // [10 -> 20 -> 30]
+        /*Node prev = null;
+        last = first;
+        while (first != null) {
+            Node next = first.next;
+            first.next = prev;
+            prev = first;
+            first = next;
+        }
+        first = prev;*/
+
+        Node prev = first;
+        Node curr = first.next;
+        while (curr != null) {
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        last = first;
+        last.next = null;
+        first = prev;
+
+    }
+
+    public int getKthFromEnd(int k) {
+        if (isEmpty()) throw new IllegalStateException();
+        Node a = first;
+        Node b = first;
+//        if (k > size) throw new IllegalArgumentException();
+        for (int i = 0; i < k - 1; i++) {
+            b = b.next;
+            if (b == null)
+                throw new IllegalArgumentException();
+        }
+        while (b.next != null) {
+            a = a.next;
+            b = b.next;
+        }
+        return a.value;
+
+    }
+
+    public void printMiddle() {
+        Node a = first;
+        Node b = first;
+        while (b.next != null && b.next.next != null) {
+            b = b.next.next;
+            a = a.next;
+        }
+
+        if (b.next == null) {
+            System.out.println(a.value);
+        } else {
+            System.out.println(a.value + ", " + a.next.value);
+        }
+    }
+
+    public boolean hasLoop() {
+        if (isEmpty()) return false;
+        Node a = first;
+        Node b = first;
+        while (b != null && b.next != null) {
+            b = b.next.next;
+            a = a.next;
+
+            if (a == b) return true;
+        }
+        return false;
+    }
+
+    private boolean isEmpty() {
+        return first == null;
+    }
+
+    public static void main(String[] args) {
+        TwoTailLinkedList list = new TwoTailLinkedList();
+
+        list.addLast(10);
+        list.addLast(20);
+        list.addLast(30);
+        list.addLast(40);
+        list.addLast(50);
+        System.out.println(Arrays.toString(list.toArray()));
+    }
+}
