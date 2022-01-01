41e4a3bf1a1ea041cf680e8baeb33d4e87ae4c51
huangmickey <916m.huang@gmail.com>
2021-12-31 19:22:15 -0800

    Added graph depth first search iterative and recurisve. Added breadth first search iterative

--- src/com/datastructures/graphs/Graph.java
+++ src/com/datastructures/graphs/Graph.java
@@ -1,9 +1,6 @@
 package com.datastructures.graphs;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 // Directed Graphs Using Adjacency List
 public class Graph {
@@ -55,6 +52,73 @@ public class Graph {
         adjacencyList.get(fromNode).remove(toNode);
     }
 
+    public void traverseDepthFirst(String root) {
+        Node node = nodes.get(root);
+        if (node == null) return;
+        traverseDepthFirst(nodes.get(root), new HashSet<>());
+    }
+
+    private void traverseDepthFirst(Node root, Set<Node> visited) {
+        System.out.println(root);
+        visited.add(root);
+        for (Node node : adjacencyList.get(root)) {
+            if (!visited.contains(node)) {
+                traverseDepthFirst(node, visited);
+            }
+        }
+    }
+
+    public void traverseDepthFirstIterative(String root) {
+        // validate that graph has the target root
+        Node node = nodes.get(root);
+        if (node == null) return;
+
+        Stack<Node> stack = new Stack<>();
+        Set<Node> visited = new HashSet<>();
+
+        // push(root)
+        stack.push(node);
+        // while(stack is not empty)
+        while (!stack.isEmpty()) {
+            //  current = pop()
+            Node current = stack.pop();
+            //  visit(current)
+            if (visited.contains(current)) continue;
+            System.out.println(current);
+            visited.add(current);
+            //  push each unvisited neighbor onto stack
+            for (Node linkedNode : adjacencyList.get(current)) {
+                if (!visited.contains(linkedNode)) {
+                    stack.push(linkedNode);
+                }
+            }
+        }
+    }
+
+    public void traverseBreadthFirstIterative(String root) {
+       Node node = nodes.get(root);
+       if (node == null) return;
+
+       Queue<Node> queue = new ArrayDeque<>();
+       Set<Node> visited = new HashSet<>();
+       queue.add(node);
+       while (!queue.isEmpty()) {
+           Node current = queue.remove();
+
+           if (visited.contains(current)) continue;
+
+           System.out.println(current);
+           visited.add(current);
+
+           for (Node neighbour : adjacencyList.get(current)) {
+               if (!visited.contains(neighbour)) {
+                   queue.add(neighbour);
+               }
+           }
+       }
+    }
+
+
     public void print() {
         for (Node sourceNode : adjacencyList.keySet()) {
             List<Node> targetLists = adjacencyList.get(sourceNode);
@@ -64,17 +128,17 @@ public class Graph {
         }
     }
 
-
     public static void main(String[] args) {
         Graph graph = new Graph();
         graph.addNode("A");
         graph.addNode("B");
         graph.addNode("C");
+        graph.addNode("D");
         graph.addEdge("A", "B");
         graph.addEdge("A", "C");
-        graph.removeNode("A");
-        graph.addEdge("B", "A");
+        graph.addEdge("B", "D");
+        graph.addEdge("D", "C");
         graph.print();
+        graph.traverseBreadthFirstIterative("A");
     }
-
 }
